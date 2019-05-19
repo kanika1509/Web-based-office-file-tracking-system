3b952e442382be6c215edbe8c4b12eb3658c61f0
kanika1509 <48168715+kanika1509@users.noreply.github.com>
2019-05-19 09:52:36 +0530

    Add files via upload

---
+++ include/Vcsidebar.php
@@ -0,0 +1,56 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span>Main Navigation</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="http://localhost/CodeIgniter-3.1.8/CodeIgniter-3.1.8/index.php/DashboardFTSContr1">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"><strong> Dashboard </strong></span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="http://localhost/CodeIgniter-3.1.8/CodeIgniter-3.1.8/index.php/VcTrackPageController">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-pencil-alt"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"><strong> Track Any File</strong> </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li>
+								<a href="http://localhost/CodeIgniter-3.1.8/CodeIgniter-3.1.8/index.php/BarChartContr">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-pencil-alt"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"><strong> Bar chart</strong> </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+
+							
+						</ul>
+						<!-- end: CORE FEATURES -->
+						
+					</nav>
+					</div>
+			</div>
---
+++ include/sidebar.php
@@ -0,0 +1,95 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span>Main Navigation</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="http://localhost/CodeIgniter-3.1.8/CodeIgniter-3.1.8/index.php/DashboardFTSContr">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"><strong> Dashboard </strong></span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="http://localhost/CodeIgniter-3.1.8/CodeIgniter-3.1.8/index.php/CreateFileFTS">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-pencil-alt"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"><strong> Create File</strong> </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li>
+								<a href="http://localhost/CodeIgniter-3.1.8/CodeIgniter-3.1.8/index.php/TrackPageContr">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"><strong> Track File</strong> </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="http://localhost/CodeIgniter-3.1.8/CodeIgniter-3.1.8/index.php/PersonalRegisterContr">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> <strong>Personal Register</strong> </span>
+										</div>
+									</div>
+								</a>
+							</li>
+		
+							<li>
+								<a href="http://localhost/CodeIgniter-3.1.8/CodeIgniter-3.1.8/sample forms/<?php echo $this->session->userdata('DeptId');?>.pdf"> 
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> <strong>Sample Form</strong> </span>
+										</div>
+									</div>
+								</a>
+
+							
+							</li>
+							<li>
+								<a href="http://localhost/CodeIgniter-3.1.8/CodeIgniter-3.1.8/index.php/FeedbackController">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> <strong> Give Feedback</strong> </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							
+
+
+						</ul>
+						<!-- end: CORE FEATURES -->
+						
+					</nav>
+					</div>
+			</div>
